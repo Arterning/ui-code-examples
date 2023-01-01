a161a7b54bbde94c6f5b0476e85408bf003547d4
Arterning <ning.huang.ridson@gmail.com>
2023-01-01 22:37:12 +0800

    add redirect

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -2,13 +2,14 @@ import App from "@/App";
 import Home from "@/views/Home";
 import About from "@/views/About";
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 
 //使用括号相当于自动return 比花括号更简洁
 const baseRouter = () => (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App/>}>
+                <Route path="/" element={<App/>}>
+                <Route path="/" element={<Navigate to={"/home"}/>}></Route>
                 <Route path="/home" element={<Home/>}></Route>
                 <Route path="/about" element={<About/>}></Route>
             </Route>
