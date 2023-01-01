795c83badf65082d1f607321da3582fe78313374
Arterning <ning.huang.ridson@gmail.com>
2023-01-01 17:27:47 +0800

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
+import Component1 from './components/Component1'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       Ning
+      <Component1/>
     </div>
   )
 }
---
+++ src/components/Component1/index.tsx
@@ -0,0 +1,11 @@
+import "./style.scss"
+
+function Component1 () {
+    return (
+        <div className="box">
+            <p>Component1</p>
+        </div>
+    )
+}
+
+export default Component1
