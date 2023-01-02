40843c39ac7c05c9ac7cf292846c2230f6452c6e
Arterning <ning.huang.ridson@gmail.com>
2023-01-02 11:06:59 +0800

    展开菜单控制

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -32,13 +32,13 @@ function getItem(
 const items: MenuItem[] = [
   getItem('about', '/about', <PieChartOutlined />),
   getItem('ning', '/ning', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
+  getItem('User', 'user', <UserOutlined />, [
     getItem('Tom', '3'),
     getItem('Bill', '4'),
     getItem('Alex', '5'),
   ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
+  getItem('Team', 'team', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', 'file', <FileOutlined />),
 ];
 
 const Home: React.FC = () => {
@@ -46,16 +46,34 @@ const Home: React.FC = () => {
 
   const navigate = useNavigate();
 
+  const [openKeys, setOpenKeys] = useState(['']);
+  
+
   const menuClick = (e:{key:string}) => {
     
     navigate(e.key)
+  }
+
+  const handleOpenChange = (keys:string[]) => {
+
+    //展开和回收菜单的时候执行这里的代码
+    //设置只能展开一个菜单
+    setOpenKeys([keys[keys.length -1]])
   }
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
         <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={menuClick}/>
+        <Menu 
+          theme="dark" 
+          defaultSelectedKeys={['/ning']}
+          mode="inline" 
+          items={items} 
+          onClick={menuClick}
+          onOpenChange={handleOpenChange}
+          openKeys={openKeys}
+        />
       </Sider>
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ paddingLeft: '16px' }}>
