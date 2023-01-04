ade21a2c100ba546ec20929d8e1fefebf70f30d9
Arterning <ning.huang.ridson@gmail.com>
2023-01-04 09:45:28 +0800

    commit

--- src/types/store.d.ts
+++ src/types/store.d.ts
@@ -1,5 +1,6 @@
 // import store from '@/store'
 // TS提供了ReturnType，用来获取函数类型的返回值
+// 全局声明 不需要引入
 type RootState = ReturnType<typeof import('@/store').getState>
 
 interface Window{
--- src/views/PageOne.tsx
+++ src/views/PageOne.tsx
@@ -14,7 +14,7 @@ const View = () => {
         // dispatch({type: 'add1'})
         dispatch({type: 'add3',val: 100})
     }
-    const changeNum2 = () => {
+    const changeNumAsync = () => {
         // 最开始的写法-同步的写法
         // dispatch({type: 'add1'})
         // 异步的写法  redux-thunk的用法  基本格式  dispatch(异步执行的函数)
@@ -39,7 +39,7 @@ const View = () => {
             <p>这是page1的内容</p>
             <p>{num}</p>
             <button onClick={changeNum}>同步按钮</button>
-            <button onClick={changeNum2}>异步按钮</button>
+            <button onClick={changeNumAsync}>异步按钮</button>
             <p>{sarr}</p>
             <button onClick={changeArr}>按钮</button>
         </div>
