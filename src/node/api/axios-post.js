b376c5791f45075de9935b194110e9f94ccd9387
Arterning <ning.huang.ridson@gmail.com>
2023-01-06 17:54:49 +0800

    axios

--- src/node/api/axios-post.js
+++ src/node/api/axios-post.js
@@ -1,21 +1,14 @@
 //import {axios} from 'axios'
 const axios = require('axios')
 
-axios({
-    method: 'GET',
-    url:'http://localhost:3000/girl'
-}).then(res => {
-    console.log(res.data);
-})
-
 
 axios({
     method: 'POST',
     url:'http://localhost:3000/girl',
     data: {
-        "id": 4,
-        "name": "大波妹 花花",
-        "size": "E 罩杯",
+        "id": 5,
+        "name": "大波妹 静静",
+        "size": "F 罩杯",
         "age": 23,
         "height": "168cm"
       }
